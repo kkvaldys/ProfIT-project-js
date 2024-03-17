04e43f837a8b29554c089ce0aea3e0c62a3e31e0
OksanaBakhaieva <obahaeva@gmail.com>
2024-03-17 19:16:40 +0000

    Update js

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,19 +1,13 @@
-// Функція, що перевіряє, чи елемент знаходиться в області видимості
 const coversSection = document.querySelector('.covers-section');
 
-
+// Функція, що перевіряє, чи елемент знаходиться в області видимості
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-    const windowWidth = window.innerWidth || document.documentElement.clientWidth;
-
-    // Перевіряємо, чи знаходиться верхній і нижній краї прямокутника всередині видимої області
-    // і чи знаходяться лівий і правий краї всередині видимої області
     return (
         rect.top >= 0 &&
-        rect.bottom <= windowHeight &&
         rect.left >= 0 &&
-        rect.right <= windowWidth
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
 
@@ -21,13 +15,11 @@ function isInViewport(element) {
 function animateIfInViewport() {
     
     if (isInViewport(coversSection)) {
-        // Знаходимо всі елементи, які потрібно анімувати
-        const elementsToAnimate = coversSection.querySelectorAll('.animate');
-        // Додаємо клас анімації до кожного елемента
-        for (let i = 0; i < elementsToAnimate.length; i++) {
-            const element = elementsToAnimate[i];
-            element.classList.add('animate');
-        }
+        const elementsToAnimate = coversSection.querySelectorAll('.covers-line');
+        // Додаємо властивість анімації до кожного елемента
+        elementsToAnimate.forEach(element => {
+            element.style.animationName = 'coversLine';
+        });
     }
 }
 
