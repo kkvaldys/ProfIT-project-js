e495e89950b9b8f8bfa18c9d3a3b8863064217c6
Vitaliia1109 <vitusechka@ukr.net>
2024-03-17 14:05:48 +0000

    js-2

--- src/js/header.js
+++ src/js/header.js
@@ -1,9 +1,7 @@
 const modalBtn = document.querySelector('.mobile-open-btn');
-
 let sidebar = document.querySelector('.mobile-menu');
 
 modalBtn.addEventListener('click', () => {
-
     sidebar.classList.add('is-open');
 }
 )
@@ -11,7 +9,27 @@ modalBtn.addEventListener('click', () => {
 const closeModalBtn = document.querySelector('.mobile-close-btn');
 
 closeModalBtn.addEventListener('click', () => {
-
     sidebar.classList.remove('is-open');
+}
+)
+
+const headerMenuBtn = document.querySelector('.toggle-menu');
+
+headerMenuBtn.addEventListener('click', () => {
+  const navMenu = document.querySelector('.menu-list');
+  navMenu.classList.toggle('active');
+}
+);
+
+const modalOrderBtn = document.querySelector('.mobile-order-btn')
+
+modalOrderBtn.addEventListener('click', () => {
+    sidebar.classList.remove('is-open');
+
+//     // Находим элемент, к которому нужно прокрутить
+// const element = document.getElementById('work');
+
+// // Выполняем плавный скролл к этому элементу
+// element.scrollIntoView({ behavior: 'smooth' });
 }
 )
