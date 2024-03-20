980a5538a80a2ce4f4ee900be81836ec803d6ad2
Andrii <kirieiev.andrii@gmail.com>
2024-03-20 23:55:13 +0200

    test about me arrow

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,3 +1,71 @@
+// import Accordion from 'accordion-js';
+// import 'accordion-js/dist/accordion.min.css';
+// import Swiper from 'swiper/bundle';
+// import 'swiper/swiper-bundle.css';
+
+// const accordion = new Accordion('.about-accordion', {
+//   onOpen: currElement => {
+//     const btnActiv = currElement.querySelector('.svg-btn');
+
+//     btnActiv.classList.toggle('clicked');
+//   },
+//   onclose: currElement => {
+//     const btnActiv = currElement.querySelector('.svg-btn');
+
+//     btnActiv.classList.toggle('clicked');
+//   },
+// });
+// accordion.open(0);
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const mySwiper = new Swiper('.about-scroll', {
+//     navigation: {
+//       nextEl: '.about-btn-left',
+//     },
+//     loop: true,
+
+//     keyboard: {
+//       enabled: true,
+//       onlyInViewport: true,
+//     },
+
+//     breakpoints: {
+//       375: {
+//         slidesPerView: 2,
+//       },
+//       768: {
+//         slidesPerView: 3,
+//       },
+//       1440: {
+//         slidesPerView: 6,
+//         spaceBetween: 0,
+//       },
+//     },
+//     on: {
+//       click: function () {
+//         if (this.isEnd) {
+//           this.slideTo(0);
+//         }
+//       },
+//     },
+//   });
+//   let previousSlide = null;
+
+//   mySwiper.on('slideChange', function () {
+//     if (previousSlide) {
+//       previousSlide.style.backgroundColor = '';
+//     }
+
+//     const activeSlide = mySwiper.slides[mySwiper.activeIndex];
+
+//     activeSlide.style.backgroundColor = '#ed3b44';
+//     activeSlide.style.borderRadius = '50%';
+//     previousSlide = activeSlide;
+//   });
+// });
+
+// ==============================
+
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 import Swiper from 'swiper/bundle';
@@ -23,12 +91,10 @@ document.addEventListener('DOMContentLoaded', function () {
       nextEl: '.about-btn-left',
     },
     loop: true,
-
     keyboard: {
       enabled: true,
       onlyInViewport: true,
     },
-
     breakpoints: {
       375: {
         slidesPerView: 2,
@@ -37,8 +103,7 @@ document.addEventListener('DOMContentLoaded', function () {
         slidesPerView: 3,
       },
       1440: {
-        slidesPerView: 6,
-        spaceBetween: 0,
+        slidesPerView: 5,
       },
     },
     on: {
@@ -49,6 +114,7 @@ document.addEventListener('DOMContentLoaded', function () {
       },
     },
   });
+
   let previousSlide = null;
 
   mySwiper.on('slideChange', function () {
