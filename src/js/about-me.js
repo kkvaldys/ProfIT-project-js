a68f765c745532f9355c8efcbcd675fe65b73d75
Andrii <kirieiev.andrii@gmail.com>
2024-03-20 22:19:49 +0200

    about me & project section update

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -3,72 +3,63 @@ import 'accordion-js/dist/accordion.min.css';
 import Swiper from 'swiper/bundle';
 import 'swiper/swiper-bundle.css';
 
-
 const accordion = new Accordion('.about-accordion', {
-    onOpen: (currElement) => {
+  onOpen: currElement => {
+    const btnActiv = currElement.querySelector('.svg-btn');
 
-        const btnActiv = currElement
-            .querySelector('.svg-btn');
-     
-        btnActiv.classList.toggle('clicked')
-    },
-    onclose: (currElement) => {
+    btnActiv.classList.toggle('clicked');
+  },
+  onclose: currElement => {
+    const btnActiv = currElement.querySelector('.svg-btn');
 
-        const btnActiv = currElement
-            .querySelector('.svg-btn');
-     
-        btnActiv.classList.toggle('clicked')
-    },
+    btnActiv.classList.toggle('clicked');
+  },
 });
-            accordion.open(0);
-
+accordion.open(0);
 
 document.addEventListener('DOMContentLoaded', function () {
-    const mySwiper = new Swiper('.about-scroll', {
-        navigation: {
-            nextEl: '.about-btn-left',
-        },
-        loop: true,
-        
-        keyboard: {
-            enabled: true,
-            onlyInViewport: true,
-        },
-        breakpoints: {
-            375: {
-                slidesPerView: 2,
-            },
-            768: {
-                slidesPerView: 3,
-            },
-            1440: {
-                slidesPerView: 6,
-            }
-        },
-        on: {
-            click: function () {
-                if (this.isEnd) {
-                    this.slideTo(0);
-                }
-            }
-        },
-    
-    
-    });
-    let previousSlide = null; 
+  const mySwiper = new Swiper('.about-scroll', {
+    navigation: {
+      nextEl: '.about-btn-left',
+    },
+    loop: true,
 
-mySwiper.on('slideChange', function () {
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+
+    breakpoints: {
+      375: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1440: {
+        slidesPerView: 6,
+        spaceBetween: 0,
+      },
+    },
+    on: {
+      click: function () {
+        if (this.isEnd) {
+          this.slideTo(0);
+        }
+      },
+    },
+  });
+  let previousSlide = null;
+
+  mySwiper.on('slideChange', function () {
     if (previousSlide) {
-        previousSlide.style.backgroundColor = '';
+      previousSlide.style.backgroundColor = '';
     }
-    
+
     const activeSlide = mySwiper.slides[mySwiper.activeIndex];
-    
+
     activeSlide.style.backgroundColor = '#ed3b44';
     activeSlide.style.borderRadius = '50%';
     previousSlide = activeSlide;
+  });
 });
-})
-
-
-
