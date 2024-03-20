64f2323375d8c38e269cad4027bfd98012ebbbbe
kkvaldys <vladbigun677@gmail.com>
2024-03-20 09:03:46 +0200

    test

--- src/js/FAQ.js
+++ src/js/FAQ.js
@@ -1,4 +1,15 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.faq-accordion');
+const accordion = new Accordion('.faq-accordion', {
+  onOpen: currElement => {
+    const btnActiv = currElement.querySelector('.svg-btn');
+
+    btnActiv.classList.toggle('clicked');
+  },
+  onclose: currElement => {
+    const btnActiv = currElement.querySelector('.svg-btn');
+
+    btnActiv.classList.toggle('clicked');
+  },
+});
