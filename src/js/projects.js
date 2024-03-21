d09796b40763b1a566807d4b4ad649589fc82d26
Anlika23 <lika44117@gmail.com>
2024-03-22 00:28:20 +0200

    fixe-bag

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,15 +1,14 @@
-
 import Swiper from 'swiper/bundle';
 import 'swiper/swiper-bundle.css';
 
 document.addEventListener('DOMContentLoaded', function () {
     const colors = {
         active: {
-            stroke: '#fafafa',
+            stroke: 'var( --text-color)',
             border: 'rgba(250, 250, 250, 0.5)'
         },
         inactive: {
-            stroke: '#3B3B3B',
+            stroke: 'var(--span-clr)',
             border: 'rgba(250, 250, 250, 0.2)' 
         }
     };
@@ -20,80 +19,37 @@ document.addEventListener('DOMContentLoaded', function () {
     prevButton.disabled = true;
     nextButton.disabled = false; 
 
-    const swiper = new Swiper('.projects-container-swiper', {
+    const projectsSwiper = new Swiper('.projects-container-swiper', {
         navigation: {
             nextEl: '.swiper-but-next',
             prevEl: '.swiper-but-prev',
         },
         keyboard: {
             enabled: true,
-            onlyInViewport: true,
-            pageUpDown: true,
         },
         slidesPerView: 1,
         spaceBetween: 30,
         speed: 800,
+        nested: true, 
 
         effect: 'cube',
         cubeEffect: {
             // slideShadows: true,
             // shadow: true,
         },
-  
     });
 
     updateButtonStates(); 
-    document.addEventListener('keydown', function (event) {
-        if (event.key === 'ArrowLeft') {
-            swiper.slidePrev();
-        } else if (event.key === 'ArrowRight') {
-            swiper.slideNext();
-        }
-    });
 
-    document.addEventListener('wheel', function (event) {
-        if (event.deltaY > 0) {
-            swiper.slideNext();
-        } else if (event.deltaY < 0) {
-            swiper.slidePrev();
-        }
-    });
-
-    function updateButtonStates() {
-        if (swiper.isBeginning) {
-            disableButton(prevButton);
-            enableButton(nextButton);
-        } else if (swiper.isEnd) {
-            enableButton(prevButton);
-            disableButton(nextButton);
-        } else {
-            enableButton(prevButton);
-            enableButton(nextButton);
-        }
-    }
-
-    // Задізейбл кнопку та змінити колір на неактивний
-    function disableButton(button) {
-        button.disabled = true;
-        button.style.borderColor = colors.inactive.border; 
-        button.querySelector('svg').style.stroke = colors.inactive.stroke;
-    }
-
-    function enableButton(button) {
-        button.disabled = false;
-        button.style.borderColor = colors.active.border; 
-        button.querySelector('svg').style.stroke = colors.active.stroke;
-    }
-
-    swiper.on('slideChange', function () {
+    projectsSwiper.on('slideChange', function () {
         updateButtonStates();
 
-        if (swiper.isBeginning) {
+        if (projectsSwiper.isBeginning) {
             prevButton.style.borderColor = colors.inactive.border;
             prevButton.querySelector('svg').style.stroke = colors.inactive.stroke;
             nextButton.style.borderColor = colors.active.border;
             nextButton.querySelector('svg').style.stroke = colors.active.stroke;
-        } else if (swiper.isEnd) {
+        } else if (projectsSwiper.isEnd) {
             prevButton.style.borderColor = colors.active.border;
             prevButton.querySelector('svg').style.stroke = colors.active.stroke;
             nextButton.style.borderColor = colors.inactive.border;
@@ -105,4 +61,29 @@ document.addEventListener('DOMContentLoaded', function () {
             nextButton.querySelector('svg').style.stroke = colors.active.stroke;
         }
     });
+
+    function updateButtonStates() {
+        if (projectsSwiper.isBeginning) {
+            disableButton(prevButton);
+            enableButton(nextButton);
+        } else if (projectsSwiper.isEnd) {
+            disableButton(prevButton);
+            disableButton(nextButton);
+        } else {
+            enableButton(prevButton);
+            enableButton(nextButton);
+        }
+    }
+
+    function disableButton(button) {
+        button.disabled = true;
+        button.style.borderColor = colors.inactive.border; 
+        button.querySelector('svg').style.stroke = colors.inactive.stroke;
+    }
+
+    function enableButton(button) {
+        button.disabled = false;
+        button.style.borderColor = colors.active.border; 
+        button.querySelector('svg').style.stroke = colors.active.stroke;
+    }
 });
